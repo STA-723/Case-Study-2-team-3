6924643c84c0fb6f7d20ee7fb9bf137b4eba3a31
Raphael Morsomme <rmorsomme@gmail.com>
2020-01-25 20:03:54 -0500

    remove old file

--- Raphael/raph_eda.R
+++
@@ -1,7 +0,0 @@
-library(tidyverse)
-d <- read_csv(
-  "AB_NYC_2019.csv",
-  col_types =  cols(
-    neighbourhood_group = col_factor(),
-    neighbourhood = col_factor(),
-    room_type = col_factor()))
